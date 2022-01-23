34597687fada4cf7c7b4c56df0520a27df85578f
Michael Zafanelli <maicomdassi@gmail.com>
2022-01-23 19:40:47 -0300

    First commit

---
+++ backend/src/main/java/com/devsuperior/backend/tarefaum/TarefaumApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.backend.tarefaum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefaumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefaumApplication.class, args);
+	}
+
+}
