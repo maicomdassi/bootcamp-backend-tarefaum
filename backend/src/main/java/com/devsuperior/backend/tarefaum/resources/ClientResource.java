98114d232308af4dd13765a923cf9b5086952726
Michael Zafanelli <maicomdassi@gmail.com>
2022-01-24 00:05:58 -0300

    findById, Delete, insert e update, mais classes de excepitions

--- backend/src/main/java/com/devsuperior/backend/tarefaum/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/backend/tarefaum/resources/ClientResource.java
@@ -1,16 +1,25 @@
 package com.devsuperior.backend.tarefaum.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.backend.tarefaum.dto.ClientDTO;
+import com.devsuperior.backend.tarefaum.entities.Client;
 import com.devsuperior.backend.tarefaum.services.ClientService;
 
 @RestController
@@ -23,13 +32,42 @@ public class ClientResource {
 	@GetMapping
 	public ResponseEntity<Page<ClientDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "5") Integer linesPerPage,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction)
+			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "Id") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction)
 	{
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		Page<ClientDTO> p = service.findAllPaged(pageRequest); 
 		return ResponseEntity.ok().body(p);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	} 
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id,@RequestBody ClientDTO dto) {
+		
+		dto = service.update(id,dto);
+		
+		return ResponseEntity.ok().body(dto);
+	} 
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Client> update(@PathVariable Long id) {
+		
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	} 
 }
--- backend/src/main/java/com/devsuperior/backend/tarefaum/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/backend/tarefaum/services/ClientService.java
@@ -1,6 +1,12 @@
 package com.devsuperior.backend.tarefaum.services;
 
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -9,18 +15,75 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.backend.tarefaum.dto.ClientDTO;
 import com.devsuperior.backend.tarefaum.entities.Client;
 import com.devsuperior.backend.tarefaum.repositories.ClientRepository;
+import com.devsuperior.backend.tarefaum.services.exceptions.DatabaseException;
+import com.devsuperior.backend.tarefaum.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
 
 	@Autowired
 	private ClientRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 
 		Page<Client> page = repository.findAll(pageRequest);
 
 		return page.map(x -> new ClientDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+
+		Optional<Client> op = repository.findById(id);
+		Client entity = op.orElseThrow(() -> new ResourceNotFoundException("Nenhum cliente encontrado"));
+
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+
+		Client entity = new Client();
+		copyDtoEntity(dto, entity);
+		entity = repository.save(entity);
+
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoEntity(dto, entity);
+			entity = repository.save(entity);
+
+			return new ClientDTO(entity);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+
+			repository.deleteById(id);
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation ");
+		}
+	}
+
+	private void copyDtoEntity(ClientDTO dto, Client entity) {
+
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+
 	}
 }
